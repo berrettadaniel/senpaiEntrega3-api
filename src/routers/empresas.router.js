bc1b7bff94fea9361ea7c59347324350546f3467
DANIEL BERRETTA <berretta.daniel@gmail.com>
2022-04-05 22:17:36 -0300

    Pasando json's a routers para armar api

--- src/routers/empresas.router.js
+++ src/routers/empresas.router.js
@@ -1,2 +1,23 @@
 const express = require('express');
 const router = express.Router();
+
+
+/*
+{
+  "empresas": [
+    {
+      "id": 1,
+      "nombre": "Arena y Portland",
+      "telefono": "099111111",
+      "idServicio": 1    //Albanileria
+    },
+    {
+      "id": 2,
+      "nombre": "Cable pelado",
+      "telefono": "099231111",
+      "idServicio": 3    //Electricidad
+    }
+]
+}
+
+*/
