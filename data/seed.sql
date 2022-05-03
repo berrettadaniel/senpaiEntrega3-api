62637a998cde05f67ec0890c702e48f426006235
DANIEL BERRETTA <berretta.daniel@gmail.com>
2022-05-03 10:27:58 -0300

    BD Paquetes DotEnv

---
+++ data/seed.sql
@@ -0,0 +1,82 @@
+CREATE TABLE public.empresas
+(
+    id bigserial primary key not null,
+    nombre character(50) NOT NULL,
+    telefono character(9),
+    idServicio integer NOT NULL,
+    email character(70)
+)
+
+INSERT INTO empresas(nombre, telefono, idServicio, email) VALUES
+	('MetaCucharin', '099111111', 1, 'consulta@metacucharin.com'),
+	('Cable pelado', '099231111', 3, 'consulta@cablepelado.com'),
+	('TablaAstillada', '099321111', 2, 'consulta@tablaastillada.com'),
+	('ClavoEnElDedo', '099421111', 2, 'consulta@clavoeneldedo.com'),
+	('ChauMosquito', '099541111', 4, 'consulta@chaumosquito.com'),
+	('CueritoFlojo', '099671111', 7, 'consulta@cueritoflojo.com');
+
+/*===================================================*/
+
+
+CREATE TABLE servicios
+(
+    id bigserial primary key not null,
+    nombre character(30) NOT NULL,
+    archivo character(100),
+    archivoTransp character(100)
+)
+
+INSERT INTO servicios (nombre, archivo, archivoTransp) VALUES
+('Albañileria', '/assets/imagesOficios/Oficio_albanil.jpg', '/assets/imagesServicios/ServiceAlbanileria_transp.png'),
+('Carpinteria', '/assets/imagesOficios/Oficio_carpintero.jpg', '/assets/imagesServicios/ServiceCarpinteria_transp.png'),
+('Electricidad', '/assets/imagesOficios/Oficio_electricista.jpg', '/assets/imagesServicios/ServiceElectricidad_transp.png'),
+('Fumigacion', '/assets/imagesOficios/Oficio_fumigador.jpg', '/assets/imagesServicios/ServiceJardineria_transp.png'),
+('Jardineria', '/assets/imagesOficios/Oficio_jardinero.jpg', '/assets/imagesServicios/ServiceJardineria_transp.png'),
+('Limpieza', '/assets/imagesOficios/Oficio_limpieza.jpg', '/assets/imagesServicios/ServicePintura_transp.png'),
+('Sanitaria', '/assets/imagesOficios/Oficio_sanitario.jpg', '/assets/imagesServicios/ServSanitaria_transp.png');
+
+/*================================================*/
+
+
+CREATE TABLE usuarios
+(
+    id bigserial primary key not null,
+    nombre character(70),
+    email character(80),
+    clave character(20)
+)
+
+INSERT INTO usuarios(id, nombre, email, clave) VALUES
+	(1, 'Daniel', 'berretta.daniel@gmail.com', '123'),
+	(2, 'Gustavo', 'gustavguez@gmail.com', '124');
+	
+/*================================================*/
+
+
+CREATE TABLE trabajos ( 
+	id bigserial primary key not null,
+	fechaInicio char(8),
+	finalizado boolean
+)
+
+insert into trabajos (fechaInicio, finalizado) values
+('20220111', true),
+('20220202', false)
+
+/*================================================*/
+
+
+CREATE TABLE tareas (
+	idTarea bigserial not null primary key,
+	idTrabajo bigint not null,
+	fecha char(8),
+	descripcion char(15),
+    idEmpresa bigint not null
+);
+
+insert into tareas (idtrabajo, fecha, descripcion, idempresa) values
+(2, '20220107', 'prueba', 2)
+(2, '20220108', 'prueba2', 2);
+
+-- A los efectos de poder probar un insert desde la UI, cualquiera sea el servicio elegido, en todos los casos
+-- considerare tareas para el codigo de empresa 2
---
+++ src/configs/db.js
@@ -0,0 +1,15 @@
+const { Pool } = require("pg");
+
+const config = {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+};
+
+const pool = new Pool(config);
+
+module.exports = {
+  query: (query, parametros) => pool.query(query, parametros),
+};
